fd7b750414088b67a99fa725093d426c0712eaeb
hiwotAB <Eliana2015>
2017-09-11 22:13:03 -0400

    cheking on template email

--- src/main/java/com/hiwotab/springboot12/Springboot12Application.java
+++ src/main/java/com/hiwotab/springboot12/Springboot12Application.java
@@ -5,13 +5,16 @@ import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import it.ozimov.springboot.mail.model.Email;
 import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
 import it.ozimov.springboot.mail.service.EmailService;
+import it.ozimov.springboot.mail.service.exception.CannotSendEmailException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEmailTools
@@ -20,6 +23,20 @@ public class Springboot12Application {
 	public static void main(final String ...args) {
 		SpringApplication.run(Springboot12Application.class, args);
 	}
+	@Autowired
+	public EmailService emailService;
 
+	public void sendEmailWithTemplating(String recipient) throws AddressException, CannotSendEmailException {
+		final Email email = DefaultEmail.builder()
+				.from(new InternetAddress("http://www.cic@mala-tem.com"))
+				.to(Lists.newArrayList(new InternetAddress("person2@m.com")))
+				.subject("a Subject")
+				.body("stuff in the body of the email")
+				.encoding("UTF-8")
+				.build();
+		final Map<String,Object> modelObject=new HashMap<>();
+		modelObject.put("recipient",recipient);
+		emailService.send(email,"emailtemp",modelObject);
+	}
 
 }
